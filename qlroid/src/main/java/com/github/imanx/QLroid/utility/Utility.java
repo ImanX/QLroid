776a432f1d1af786bcf8078e8da761051c12b0f1
imanx <Imannamix@gmail.com>
2018-08-25 16:53:04 +0430

    Fix Funckiiiiiiing shit

--- qlroid/src/main/java/com/github/imanx/QLroid/utility/Utility.java
+++ qlroid/src/main/java/com/github/imanx/QLroid/utility/Utility.java
@@ -32,7 +32,7 @@ public class Utility {
             }
 
             JSONArray  optJSONArray  = jsonObject.optJSONArray(key);
-            JSONObject optJsonObject = jsonObject.getJSONObject(key);
+            JSONObject optJsonObject = jsonObject.optJSONObject(key);
 
 
             if (optJSONArray != null) {
